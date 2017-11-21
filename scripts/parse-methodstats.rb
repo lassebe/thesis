3bb70ff10004a0fa534a2b4d7f0af2750f245cb6
Lasse <lassebe@kth.se>
2017-11-21 16:50:55 +0100

    updated parsemethodstats, renamed stats_all because its not a ruby file

--- scripts/parse-methodstats.rb
+++ scripts/parse-methodstats.rb
@@ -1,16 +1,16 @@
 require "rubygems"
 require "json"
 
-input_file = File.read('sample.json')
 
 #JSON.parse(input_file)
 stats = JSON.parse(STDIN.gets)
 
 #interrupting_methods = Hash.new(0)
-puts "name\trecorded\ttotal calls\tcontext match\tratio (match/total)\tinterrupting native call"
+puts "name\trecorded\ttotal calls\tcontext match\tratio (match/total)\tinterruption"
 
-stats["methodStats"].sort_by { |s| [s["counter"]["recorded"] ? 1 : 0, s["counter"]["totalCalls"]] }.reverse.each do |methodStats|
+stats["methodStats"].sort_by { |s| [s["counter"]["recorded"] ? 1 : 0, s["counter"]["argsMatchCount"], s["counter"]["totalCalls"]] }.reverse.each do |methodStats|
   counter = methodStats["counter"]
+  #next if counter["argsMatchCount"] == 0
   #if counter["interruptedByNativeCall"] 
     #interrupting_methods[counter["interruptingMethod"]] += 1
   #end
